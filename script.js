e32ecf6d7d93ecb22a87eab0e54871754c00eeb9
Herivelto-Brito <herivelto_brito17@outlook.com>
2023-02-18 10:20:44 -0300

    Ecommerce_Js

---
+++ script.js
@@ -0,0 +1,11 @@
+function slider (anything){
+    document.querySelector('.select') .src = anything;
+};
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle ('bx-x');
+    navbar.classList.toggle('open');
+}
